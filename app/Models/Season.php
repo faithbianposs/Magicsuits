abe2e677e536deaac803264f60b4768cf79b86bb
Frank <119316605+faithbianposs@users.noreply.github.com>
2024-06-24 22:55:39 -0500

    Segundo commit
    Segundo commit

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Season extends Model
 {
     use HasFactory;
 
-    protected $guarded=['id'];
-
-    public function posts(){
-        return $this->hasMany(Post::class);
+    public function managers(){
+        return $this->hasMany(Manager::class);
     }
 }
